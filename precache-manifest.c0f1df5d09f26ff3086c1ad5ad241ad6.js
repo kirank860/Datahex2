63228a6401294964d82c2129d75697aac08525e2
kirank860 <kkiru889@gmail.com>
2023-06-19 11:18:35 +0530

    Updates

---
+++ precache-manifest.c0f1df5d09f26ff3086c1ad5ad241ad6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "81917af5bc78d7b3f14f",
+    "url": "/Datahex-website/static/css/main.6a8aa558.chunk.css"
+  },
+  {
+    "revision": "81917af5bc78d7b3f14f",
+    "url": "/Datahex-website/static/js/main.81917af5.chunk.js"
+  },
+  {
+    "revision": "770f6a3eed8fbd744695",
+    "url": "/Datahex-website/static/css/1.cf7b7971.chunk.css"
+  },
+  {
+    "revision": "770f6a3eed8fbd744695",
+    "url": "/Datahex-website/static/js/1.770f6a3e.chunk.js"
+  },
+  {
+    "revision": "eb3027df98b95ec7ab1e",
+    "url": "/Datahex-website/static/js/runtime~main.eb3027df.js"
+  },
+  {
+    "revision": "24d5ee2b9980bc7328d7dd39aa186909",
+    "url": "/Datahex-website/index.html"
+  }
+];
